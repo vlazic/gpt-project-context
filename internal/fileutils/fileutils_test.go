68b3600c612c307f16c8b78f11eda1a39d374be3
Vladimir Lazić <contact@vlazic.com>
2023-05-28 14:25:28 +0200

    fix: update internal/fileutils/fileutils_test.go to include */*_test.go pattern

--- internal/fileutils/fileutils_test.go
+++ internal/fileutils/fileutils_test.go
@@ -25,7 +25,7 @@ func TestFilterFiles(t *testing.T) {
 			name:              "include *.go and *.md and Makefile, exclude src_*.js",
 			includePatterns:   []string{"*.go", "*.md", "Makefile"},
 			excludePatterns:   []string{"src/*.js"},
-			expectedFilePaths: []string{"src/source.go", "main.go", "helper.go", "README.md", "Makefile"},
+			expectedFilePaths: []string{"src/source.go", "main.go", "helper.go", "README.md", "Makefile", "src/source_test.go"},
 		},
 		{
 			name:              "include only *.js files and Makefile",
@@ -37,13 +37,19 @@ func TestFilterFiles(t *testing.T) {
 			name:              "include *.go files, exclude specific go file",
 			includePatterns:   []string{"*.go"},
 			excludePatterns:   []string{"main.go"},
-			expectedFilePaths: []string{"src/source.go", "helper.go"},
+			expectedFilePaths: []string{"src/source.go", "helper.go", "src/source_test.go"},
 		},
 		{
 			name:              "include *.go files, exclude go files in src folder",
 			includePatterns:   []string{"*.go"},
 			excludePatterns:   []string{"src/*.go"},
 			expectedFilePaths: []string{"main.go", "helper.go"},
+		},
+		{
+			name:              "include *.go files, exclude *_test.go files",
+			includePatterns:   []string{"*.go"},
+			excludePatterns:   []string{"*/*_test.go"},
+			expectedFilePaths: []string{"src/source.go", "main.go", "helper.go"},
 		},
 	}
 
@@ -90,13 +96,14 @@ func setupMockProject() (string, error) {
 
 	// Define the file structure
 	files := map[string]string{
-		"bin/binary_file": "",
-		"src/source.go":   "package src",
-		"src/helper.js":   "console.log('Hello, world!');",
-		"main.go":         "package main",
-		"helper.go":       "package main",
-		"README.md":       "# Test Project",
-		"Makefile":        "",
+		"bin/binary_file":    "",
+		"src/source.go":      "package src",
+		"src/source_test.go": "package main",
+		"src/helper.js":      "console.log('Hello, world!');",
+		"main.go":            "package main",
+		"helper.go":          "package main",
+		"README.md":          "# Test Project",
+		"Makefile":           "",
 	}
 
 	// Create the files
